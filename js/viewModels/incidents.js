6933376be7c1d6291e009ec1c461a87d495260b4
Srikanth Kante <srikanth.kante@oracle.com>
2021-03-15 11:49:43 +0530

    Updates

---
+++ js/viewModels/incidents.js
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2014, 2021, Oracle and/or its affiliates.
+ * Licensed under The Universal Permissive License (UPL), Version 1.0
+ * as shown at https://oss.oracle.com/licenses/upl/
+ * @ignore
+ */
+define(["accUtils"],(function(n){return function(){this.connected=()=>{n.announce("Incidents page loaded.","assertive"),document.title="Incidents"},this.disconnected=()=>{},this.transitionCompleted=()=>{}}}));
